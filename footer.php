7ddfb2d4642c886ed7723a1309fd129dc7c1fa8b
Arturo Cardenas Naranjo <ing.arturocardenas@gmail.com>
2017-07-09 14:56:35 -0700

    Agregando header y footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+		<footer>
+			<small>ArcanoThemeWP. <?php echo date("Y"); ?></small>
+		</footer>
+		<?php wp_footer(); ?>
+	</body>
+</html>	
---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo('language'); ?>">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php bloginfo('name'); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+		<?php wp_head(); ?>
+	</head>
+	<body>
